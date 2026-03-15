a675c520b19497b4dc737248d428e03435a7e188
Krittaphat Samrit <eriss.k405@gmail.com>
2026-03-16 04:07:44 +0700

    update():serverExternal

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: [],
+  serverExternalPackages: ['@imgly/background-removal-node'],
   images: {
     remotePatterns: [
       {
